0a0bb422b2c4770b2697b86e5a660b496cf83255
Fathan Maulana <94539481+FlyingCat17@users.noreply.github.com>
2022-06-03 04:14:37 +0700

    Add files via upload

---
+++ src/Swing/ScrollBarCustom.java
@@ -0,0 +1,15 @@
+package Swing;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JScrollBar;
+
+public class ScrollBarCustom extends JScrollBar {
+
+    public ScrollBarCustom() {
+        setUI(new ModernScrollBarUI());
+        setPreferredSize(new Dimension(8, 8));
+        setForeground(new Color(253, 144, 39));
+        setBackground(new Color(238, 238, 238));
+    }
+}
